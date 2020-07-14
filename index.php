d837871fcdc82aa562cf5a627a32b30f3e66a773
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-07-14 10:03:21 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE Html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BeSilent</title>
+    <link href="./landing.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+    <div>
+        <?php include './landing.php' ?>
+    </div>
+    
+    <!-- <div class="main">
+        <div class="lower">
+            <img src="./images/Vector (19).png" width="100%">
+
+        </div>
+        <div class="upper">
+            <img src="./images/Vector (18).png" width="100%">
+        </div>
+        <div class="write">
+            <h2>wsidom</h2>
+            <img src="./images/Vector 4.png" width="100px">
+        </div>
+    </div> -->
+    
+</body>
+</html>
