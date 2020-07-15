abad596a6b131bf4ad01849e799e3ffb86c8c43c
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-07-15 11:26:01 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-<!DOCTYPE Html>
+<!Doctype Html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,12 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>BeSilent</title>
     <link href="./landing.css" rel="stylesheet" type="text/css">
+    <link href="./bootstrap-4.4.1-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 
 </head>
 <body>
-    
-    
-    <div class="main">
+    <section class="main">
         <div class="lower">
             <img src="./images/Vector (19).png" width="100%">
 
@@ -20,9 +19,52 @@
         <div class="upper">
             <img src="./images/Vector (18).png" width="100%">
         </div>
-    </div>
-    <div>
-        <?php include './landing.php' ?>
-    </div>
+        <div class="text text-white">
+            <div>
+                <h1 class="logo"> BeSilent</h1>
+            </div>
+            <div class="main-flex d-flex">
+                <div class="main-flex1 col-12 col-sm-6 mb-5">
+                    <h1>Your personal automatic phone silent-mode app</h1>
+                    <p>BeSilent automatically puts your phone on silent mode based on your location, especially during meetings, interviews or in office.</p>
+                    <h1 class="mb-2 soon">Coming Soon !!!</h1>
+                    <h4 class="mb-3">Want to be among the first to launch the app?</h4>
+                    <form method="post" action="./landing.php">
+                        <div class="row forms"> 
+                            <input type="email" name="email" placeholder="Your Email Address" >
+                            <input class="submit" type="submit" name="submit" value="Get Notified">
+                        </div>
+                    </form>
+                </div>
+                <div class="main-flex2 col-12 col-sm-6">
+                    <img src="./images/Group 119.png" width="50%;" class="float-right mr-4">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="mobile-hero">
+        <div class="container py-3">
+            <h1 class="logo-mobile">BeSilent</h1>
+        </div>
+        <div class="container-fluid mobile-hero-img pb-3">
+            <img src="./images/f3f2fc6ca87084f9c1ee203a370be0c9 1.png" width="100%">
+        </div>
+        <div class="container">
+            <h1 class="mb-3 text-center">Your personal automatic phone silent-mode app</h1>
+            <p class="mb-5 text-center">BeSilent automatically puts your phone on silent mode based on your location, especially during meetings, intterviews or in office,.</p>
+            <h1 class="mb-2 text-center">Coming Soon!!!</h1>
+            <h4 class="mb-3 text-center">Want to be among the first to launch the app?</h4>
+            <form method="post" action="./landing.php">
+                <input type="email" name="email"  placeholder="Your email address" /><br>
+                <input type="submit" name="submit" value="Get Notified" class="mobile-submit mt-3 mb-5" />
+            </form>
+        </div>
+    </section>
+    <section id="features">
+        
+    </section>
+    <script src="./jquery-3.5.1.min.js"></script>
+    <script src="./popper.min.js"></script>
+    <script src="./bootstrap-4.4.1-dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- landing.php
+++ landing.php
@@ -4,41 +4,22 @@ $email = "";
 if(isset($_POST['submit']))
 {
     $email = $_POST['email'];
-    $conn = new mysqli("nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "zsf0luis4r0emanv", "kmh3vl4zg6iuc6bk", "pckf8ocyung0ua7u");
-    $cmd = "INSERT into email value ('$email');";
-    if(mysqli_query($conn, $cmd))
+    $conn = new mysqli("nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306", "zsf0luis4r0emanv", "kmh3vl4zg6iuc6bk", "pckf8ocyung0ua7u");
+    $sql = "INSERT INTO email value ('$email')";
+    if(mysqli_query($conn, $sql))
     {
         echo "successfully";
     }
     else 
     {
-        echo "not successful";
+        echo "not succeful";
     }
 }
 ?>
-
-<form method=post>
-    <input type="email" name="email" />
-    <input type="submit" name="submit" />
-</form>
 
 <div>
-<?php
-
-$conn = new mysqli("nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "zsf0luis4r0emanv", "kmh3vl4zg6iuc6bk", "pckf8ocyung0ua7u");
-$sql = "SELECT DISTINCT * FROM email";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br>- Email: ". $row["email"]. " " ;
-    }
-} else {
-    echo "0 results";
-}
-
-$conn->close();
-?>
-
+    <h1>
+    You'll be notified when the app gets launched thanks alot!
+    YEYYYY!!!
+</h1>
 </div>
