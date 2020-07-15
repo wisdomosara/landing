d5c2176460275466886569007b5a7de987a493ad
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-07-15 13:09:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
 <body>
     <section class="main">
         <div class="lower">
-            <img src="./images/Vector (19).png" width="100%">
+            <img src="./images/Vector (19).png"  width="1700px">
 
         </div>
         <div class="upper">
             <img src="./images/Vector (18).png" width="100%">
         </div>
-        <div class="text text-white">
+        <div class="text text-white mb-5">
             <div>
                 <h1 class="logo"> BeSilent</h1>
             </div>
@@ -60,8 +60,55 @@
             </form>
         </div>
     </section>
-    <section id="features">
-        
+    <section id="features" class="my-4 py-3">
+        <div class="container d-flex align-items-center flex-column">
+            <h2 class="text-center">Features</h2>
+            <div class="line text-center mb-5"></div>
+        </div>
+        <div class="container-fluid d-flex  flex-wrap align-items-center px-sm-5 mb-5">
+            <div class="col-12 col-sm-6">
+                <img src="./images/undraw_mobile_interface_wakp 1.png" width="100%">
+            </div>
+            <div class="col-12 col-sm-6">
+                <h2>Clear & Simple User Interface</h2>
+                <p>Easy to navigate and interact with. It makes it easy for you to know what to do at every point in the app. Unnecessary elements are avoided. The app is straight-forward to make the experience a memorable one for you.</p>
+            </div>
+        </div>
+        <div class="container-fluid d-flex  flex-wrap align-items-center px-sm-5 mb-5" style="background-color:#F9F9F9">
+            <div class="col-12 col-sm-6 order-sm-12">
+                <img src="./images/undraw_my_current_location_om7g 1.png" width="100%">
+            </div>
+            <div class="col-12 col-sm-6 order-sm-1 text-right">
+                <h2>Silent mode based on set locations</h2>
+                <p>You can set locations where on appearance in those locations, your phone automatically switches to silent mode. After leaving the location, it switches back to Ringer mode.</p>
+            </div>
+        </div>
+        <div class="container-fluid d-flex  flex-wrap align-items-center px-sm-5 mb-5">
+            <div class="col-12 col-sm-6">
+                <img src="./images/undraw_chat_bot_kli5 1.png" width="100%">
+            </div>
+            <div class="col-12 col-sm-6">
+                <h2 >Phone auto-silent mode using noise analysis</h2>
+                <p>Office, meetings, interviews, corporate events and religious environements often have surrounding noise levels of less than 100dB. Using noise analysis, the phone detects your surrounding noise level and switches sutomatically to silent mode</p>
+            </div>
+        </div>
+        <div class="container-fluid d-flex  flex-wrap align-items-center px-sm-5 mb-5">
+            <div class="col-12 col-sm-6 order-sm-12">
+                <img src="./images/undraw_calendar_dutt 1.png" width="100%">
+            </div>
+            <div class="col-12 col-sm-6 order-sm-1 text-right">
+                <h2 >Sync calendar events with app using personal emails</h2>
+                <p>This app allows you to sync your Google, Yahoo and Outlook emails with the app and automatically, your calendar events are synced with timed silent mode. </p>
+            </div>
+        </div>
+    </section>
+    <section id="people">
+        <div class="lower">
+            <img src="./images/Vector (20).png">
+        </div>
+        <div class="upper">
+                <img src="./images/Vector (21).png">
+        </div>
     </section>
     <script src="./jquery-3.5.1.min.js"></script>
     <script src="./popper.min.js"></script>
